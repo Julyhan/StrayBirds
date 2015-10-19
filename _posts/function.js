794fd7278ea0df3e3a4a32afcec9107c61bc49c1
Julyhan <july0404@sina.cn>
2015-10-19 10:38:06 +0800

    Update and rename qrcode.js to function.js

--- _posts/function.js
+++ _posts/function.js
@@ -1,3 +1,80 @@
+var strbegin;
+function locking(str){
+	document.body.innerHTML+=str;
+	document.all.ly.style.display="block";
+	document.all.ly.style.width=document.body.clientWidth;
+	document.all.ly.style.height=document.body.clientHeight;
+	document.all.Layer2.style.display='block';
+
+}
+function Lock_CheckForm(){	
+	document.all.ly.style.display='none';
+	document.all.Layer2.style.display='none';
+	document.body.innerHTML=strbegin;
+	return false;
+}
+function openlog()
+{
+	open('bbslform','','left=255,top=190,width=130,height=100');
+}
+function eva(board, file) {
+	var s;
+	s=" [<a href=eva?B="+board +"&F="+file+"&star=";
+	document.writeln("喜欢这个文章么? 这个文章"+s+"1>不错</a>]"+s+"3>很好</a>]"+s+"5>强烈推荐!</a>]");
+}
+function docform(a, b){
+	document.writeln("<table border=0 cellspacing=0 cellpading=0><tr><td><form name=docform1 action="+a+"><a href="+a+"?B="+b+"&S=1>第一页</a> <a href="+a+"?B="+b+"&S=0>最后一页</a> <input type=hidden name=B value="+b+"><input type=submit value=转到>第<input type=text name=start size=4>篇</form></td><td><form name=docform2 action="+a+"><input type=submit value=转到><input type=text name=B size=7>讨论区</form></td></tr></table>");
+}
+function share(a,title,board,file){
+    var url;
+    switch(a){
+        case 'sina':
+            url = "http://v.t.sina.com.cn/share/share.php?appkey=3235716756&title=%23BMYBBS%E8%AF%9D%E9%A2%98%E5%88%86%E4%BA%AB%23"+title+"&url=http://bbs.xjtu.edu.cn/BMY/con?B="+board+"%26F="+file;
+            break;
+        case 'renren':
+            url = "http://share.renren.com/share/buttonshare.do?link=http%3A%2F%2Fbbs.xjtu.edu.cn%2FBMY%2Fcon%3FB%3D"+board+"%26F%3D"+file;
+            break;
+        case 'tencent':
+            url = "http://share.v.t.qq.com/index.php?c=share&a=index&title=%23BMYBBS%E8%AF%9D%E9%A2%98%E5%88%86%E4%BA%AB%23"+title+"&url=http%3A%2F%2Fbbs.xjtu.edu.cn%2FBMY%2Fcon%3FB%3D"+board+"%26F%3D"+file+"&appkey=801082141&pic=&assname";
+            break;
+        default:
+            break;
+    }
+    var popup=window.open(url);
+    popup.focus();
+}
+
+function sharewechat(){
+	strQr=window.location.protocol + '//' + window.location.pathname;
+	strbegin = document.body.innerHTML;
+	var sss=create_qrcode(strQr, 4, 'L', 6);
+	var str = "<div id='ly' style='position: absolute; top: 0px; filter: alpha(opacity=60); background-color: #777;z-index: 2; left: 0px; display: none;'></div>"
+				+"<div id='Layer2' align='center' style='position: relative; z-index: 3;  width:540px; margin-left:auto; margin-right:auto; background-color: #fff; display: none;' >"
+				+"<table width='540' height='300' border='0' cellpadding='0' cellspacing='0' style='border: 0    solid    #e7e3e7;border-collapse: collapse ;' >"
+				+"<tr><td style='background-color: #73A2d6; color: #fff; padding-left: 4px; padding-top: 2px;font-weight: bold; font-size: 12px;' height='10' valign='middle'>"
+				+"<div align='right'><a href=JavaScript:; class='STYLE1' onclick='Lock_CheckForm();'>[close]</a></div></td></tr>"
+				+"<tr><td height='130' align='center'><script src='qrcode.js'></script>"
+				+"<style> .ykewm{border-width: 0px; border-style: none;border-collapse: collapse;padding: 0px;}.ykewm td{border-width: 0px; border-style: none;border-collapse: collapse;padding: 0px; margin: 0px;}</style><div id='qr'></div>"
+				+sss
+				+"</td></tr>"
+				+"</table></div>";
+	locking(str);
+}
+function thread_share(a,title,owner,board,thread,start,article_count){
+    var url;
+    switch(a){ ////BMY%2Fbbstcon%3Fboard%3D{XJTUenp}%26start%3D{10}%26th%3D{1333036729}%23{0}
+        case 'sina':
+            url = "http://v.t.sina.com.cn/share/share.php?appkey=3235716756&title=%23BMYBBS%E8%AF%9D%E9%A2%98%E5%88%86%E4%BA%AB%23%E6%88%91%E8%A7%89%E5%BE%97"+owner+"%E7%BD%91%E5%8F%8B%E5%9C%A8%E3%80%8A"+title+"%E3%80%8B%E4%B8%AD%E7%9A%84%E5%8F%91%E8%A8%80%E5%BE%88%E7%B2%BE%E5%BD%A9%EF%BC%8C%E4%BD%A0%E4%B9%9F%E5%BA%94%E8%AF%A5%E6%9D%A5%E7%9C%8B%E4%B8%80%E4%B8%8B&url=http://bbs.xjtu.edu.cn/BMY/bbstcon%3Fboard%3D"+board+"%26start%3D"+start+"%26th%3D"+thread+"%23"+article_count;
+            break;
+        case 'tencent':
+            url = "http://share.v.t.qq.com/index.php?c=share&a=index&title=%23BMYBBS%E8%AF%9D%E9%A2%98%E5%88%86%E4%BA%AB%23%E6%88%91%E8%A7%89%E5%BE%97"+owner+"%E7%BD%91%E5%8F%8B%E5%9C%A8%E3%80%8A"+title+"%E3%80%8B%E4%B8%AD%E7%9A%84%E5%8F%91%E8%A8%80%E5%BE%88%E7%B2%BE%E5%BD%A9%EF%BC%8C%E4%BD%A0%E4%B9%9F%E5%BA%94%E8%AF%A5%E6%9D%A5%E7%9C%8B%E4%B8%80%E4%B8%8B&url=http%3A%2F%2Fbbs.xjtu.edu.cn%2FBMY%2Fbbstcon%3Fboard%3D"+board+"%26start%3D"+start+"%26th%3D"+thread+"%23"+article_count+"&appkey=801082141&pic=&assname";
+            break;
+        default:
+            break;
+    }
+    var popup=window.open(url);
+    popup.focus();
+}
 //---------------------------------------------------------------------
 //
 // QR Code Generator for JavaScript
